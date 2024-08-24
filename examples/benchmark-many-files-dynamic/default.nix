ee66d2c844dcca2f3556d5749feb837263ffc00d
Farid Zakaria <farid.m.zakaria@gmail.com>
2024-08-24 14:08:05 -0700

    Made it a symlink join

--- examples/benchmark-many-files-dynamic/default.nix
+++ examples/benchmark-many-files-dynamic/default.nix
@@ -1,53 +1,53 @@
-{ stdenv, python3, lib }:
-let fs = lib.fileset;
-in stdenv.mkDerivation {
-  name = "raw_functions_and_libraries";
-  nativeBuildInputs = [ python3 ];
-  src = fs.toSource {
-    root = ./.;
-    fileset = fs.unions [ ./Makefile ./generate_sources.py ];
-  };
-  dontStrip = true;
-  NIX_CFLAGS_COMPILE = "-g -O0";
-  buildPhase = ''
-    # Function to generate the shared objects and executable
-    generate_files() {
-        local num_functions=$1
-        local num_shared_objects=$2
-        local total_symbols=$((num_functions * num_shared_objects))
-        echo "Generating for $total_symbols symbols: $num_functions functions per shared object, $num_shared_objects shared objects"
-
-        mkdir build_''${num_functions}_''${num_shared_objects}
-        pushd build_''${num_functions}_''${num_shared_objects}
-        python3 $src/generate_sources.py $num_functions $num_shared_objects
-
+{
+  symlinkJoin,
+  stdenv,
+  python3,
+  lib,
+}: let
+  fs = lib.fileset;
+  # Define the possible values for total_functions and num_shared_objects
+  num_functions = [1 10 100 1000 10000 100000 1000000];
+  num_shared_objects = [1 10 100 1000 10000 100000 1000000];
+  combinations =
+    builtins.filter (combination: (combination.functions * combination.shared_objects) <= 1000000)
+    (lib.lists.zipListsWith (
+      functions: shared_objects: {
+        functions = functions;
+        shared_objects = shared_objects;
+      }
+    ) num_functions num_shared_objects);
+  buildBinary = combination: let
+    num_functions = toString combination.functions;
+    num_shared_objects = toString combination.shared_objects;
+  in
+    stdenv.mkDerivation {
+      name = "${num_functions}_${num_shared_objects}_raw_functions_and_libraries";
+      nativeBuildInputs = [python3];
+      src = fs.toSource {
+        root = ./.;
+        fileset = fs.unions [./Makefile ./generate_sources.py];
+      };
+      dontStrip = true;
+      NIX_CFLAGS_COMPILE = "-g -O0";
+      buildPhase = ''
+        python3 $src/generate_sources.py ${num_functions} ${num_shared_objects}
         echo "Building with ''${NIX_BUILD_CORES} cores"
         make -f $src/Makefile -j''${NIX_BUILD_CORES}
-        # Rename the benchmark executable
-        mv benchmark benchmark_''${num_functions}_''${num_shared_objects}
-        popd
-    }
+        mv benchmark benchmark_${num_functions}_${num_shared_objects}
+      '';
 
-    # set a maximum number of functions and shared objects
-    # larger than this seems to cause building to take too long
-    max_functions=1000000
+      installPhase = ''
+        mkdir -p $out/bin
+        mkdir -p $out/lib
 
-    for num_functions in 1 10 100 1000 10000 100000 1000000; do
-      for num_shared_objects in 1 10 100 1000 10000 100000 1000000; do
-          total_functions=$((num_functions * num_shared_objects))
-          if [[ $total_functions -gt $max_functions ]]; then
-            continue
-          fi
-          generate_files $num_functions $num_shared_objects
-      done
-    done
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    mkdir -p $out/lib
-
-    mv build_*/*.so $out/lib
-    mv build_*/benchmark_*_* $out/bin
-  '';
+        mv *.so $out/lib
+        mv benchmark_*_* $out/bin
+      '';
+    };
+  # Create a list of derivations for the filtered combinations
+  binaries = map (combination: buildBinary combination) combinations;
+in
+symlinkJoin {
+  name = "raw_functions_and_libraries";
+  paths = [binaries];
 }
